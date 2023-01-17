412ed9d336cb8b12da689b3f4caf051262eba71c
JGuyCoder <joshuapankratz07@gmail.com>
2023-01-17 18:21:56 -0500

    Update Sensors.java

--- Illuminati-Bot/src/main/java/frc/robot/subsystems/Sensors.java
+++ Illuminati-Bot/src/main/java/frc/robot/subsystems/Sensors.java
@@ -12,12 +12,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Sensors extends SubsystemBase {
   /** Creates a new Sensors. */
   public Sensors() {}
-    //Limelight Networktable collection
-    
+  //Limelight Networktable collection
    public static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+   //tx and ty are in degrees, the distance from scoring posts
    public static NetworkTableEntry tx = table.getEntry("tx");
    public static NetworkTableEntry ty = table.getEntry("ty");
+   //ta is the rough distance from the apriltags
    public static NetworkTableEntry ta = table.getEntry("ta");
+   //tid is the apriltag the limelight is currently locked on
    public static NetworkTableEntry tid = table.getEntry("tid");
   @Override
   public void periodic() {
